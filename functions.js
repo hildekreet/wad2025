757c8a6206347ea2e9bfc82f5345285363085d68
Häli <halimai@ut.ee>
2025-11-04 21:17:32 +0200

    fetch posts

--- functions.js
+++ functions.js
@@ -1,3 +1,26 @@
+const endpoint = "https://jsonkeeper.com/b/3BI3A";
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        const posts = data.Posts;
+        const container = document.getElementById("postsContainer");
+
+        posts.forEach(post => {
+            const postDiv = document.createElement("div");
+            postDiv.classList.add("post");
+
+            postDiv.innerHTML = `
+            <h2>${post.title}</h2>
+            <p><strong>Author:</strong> ${post.author}</p>
+            <p>${post.content}</p>
+            `;
+
+            container.appendChild(postDiv);
+        });
+    })
+    .catch(error => console.error("Viga andmete laadimisel:", error));
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
